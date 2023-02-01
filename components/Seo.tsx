322e7eba2f4b9a6f3dc996fd2780d1cc1be50210
Joefs <joefstack@gmail.com>
2023-01-31 23:56:19 -0500

    :mag: added header :mag:

---
+++ components/Seo.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+
+type SeoProps = {
+  pageTitle?: string;
+};
+const Seo = ({pageTitle}: SeoProps) => {
+  return (
+    <>
+      <Head>
+        <title>{`Joseph Chow${pageTitle && ` - ${pageTitle}`}`}</title>
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="keywords"
+          content="portfolio, developer, resume, cv, react, nextjs"
+        />
+        <meta
+          name="description"
+          content="Joseph Chow - My personal work portfolio"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link rel="icon" href="my-favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Seo;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
-import Head from 'next/head';
+import Seo from '@/components/Seo';
+import {Heading} from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Joseph Chow</title>
-        <meta name="description" content="My personal work portfolio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/my-favicon.ico" />
-      </Head>
+      <Seo pageTitle="Portfolio" />
       <main>
-        <h1>Hello World 2023!!</h1>
+        <Heading>Hello World 2023!!</Heading>
       </main>
     </>
   );
