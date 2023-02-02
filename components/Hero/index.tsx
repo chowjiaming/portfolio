eaebeda887e5d95801e31d598b8c67686d5c0ab1
Joseph Chow <contact@josephchow.dev>
2023-02-02 02:25:49 -0500

    :construction: scaffold basic hero :construction:

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,4 +1,7 @@
-import {Flex, Heading} from '@chakra-ui/react';
+import {Box, Button, Flex, Heading, Text} from '@chakra-ui/react';
+import {heroContent} from '@/data/hero';
+import Image from 'next/image';
+import ReactTyped from 'react-typed';
 
 export default function Hero(): JSX.Element {
   return (
@@ -10,7 +13,42 @@ export default function Hero(): JSX.Element {
       direction="column"
       height="100vh"
     >
-      <Heading>Hero Section</Heading>
+      <Image
+        src={heroContent.heroImage}
+        alt="Profile picture"
+        width={500}
+        height={500}
+      />
+      <Heading
+        as="h1"
+        fontSize={{base: '3xl', md: '4xl', lg: '5xl'}}
+        fontWeight="extrabold"
+        textAlign="center"
+        color="gray.900"
+        mt={4}
+      >
+        Hello, I&apos;m {heroContent.name}
+      </Heading>
+      <Box>
+        <ReactTyped
+          loop
+          typeSpeed={150}
+          backSpeed={60}
+          strings={['Front End Developer', 'Photographer']}
+          smartBackspace
+          shuffle={false}
+          backDelay={1}
+          fadeOut={false}
+          fadeOutDelay={100}
+          loopCount={0}
+          showCursor
+          cursorChar="|"
+        />
+      </Box>
+      <Text>{heroContent.description}</Text>
+      <Button as={'a'} variant={'link'} href="/img/profilepic.jpg" download>
+        Download CV
+      </Button>
     </Flex>
   );
 }
---
+++ data/hero.ts
@@ -0,0 +1,6 @@
+export const heroContent = {
+  shapeImage: '/img/profilepic.jpg',
+  heroImage: '/img/profilepic.jpg',
+  name: 'Joseph',
+  description: `I'm a Front End Web Developer based in Toronto, Canada. I'm passionate about building beautiful, responsive websites and applications. I'm currently looking for a full-time position as a Front End Developer.`,
+};
