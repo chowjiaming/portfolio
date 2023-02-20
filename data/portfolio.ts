dedf2cb7b7ce311d59e5aaa8c814ca4a7cc6b7a6
Joseph Chow <contact@josephchow.dev>
2023-02-20 02:59:41 -0500

    :fire: removed react-slick :fire:

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -4,7 +4,15 @@ export const intro = {
     'Here is a showcase of my work and demonstrate the impact that I have made as a front-end developer. Each project is a testament to my skills, creativity, and commitment to delivering high-quality solutions. I have had the opportunity to work on a variety of projects, ranging from a simple SPA to complex web applications, and have consistently exceeded expectations. My projects speak for themselves, and I am confident that they will give you a good idea of the level of quality that I can bring to your team. Take a look and see what I can do for you!',
 };
 
-export const content = [
+export type Project = {
+  name: string;
+  screenshot: string;
+  github: string;
+  link: string;
+  shortDesc: string;
+  longDesc: string;
+};
+export const content: Project[] = [
   {
     name: 'Tip Calculator App',
     screenshot: '/img/screenshots/tip-calculator-screenshot.png',
--- utils/settings.ts
+++ utils/settings.ts
@@ -31,27 +31,3 @@ export const enlargeAndSpin = {
   scale: [1, 2, 2, 1, 1],
   rotate: [0, 0, 270, 270, 0],
 };
-
-export const sliderSettings = {
-  dots: true,
-  lazyLoad: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  initialSlide: 1,
-  focusOnSelect: true,
-  responsive: [
-    {
-      breakpoint: 1280,
-      settings: {
-        slidesToShow: 1,
-        arrow: false,
-        autoplay: false,
-        draggable: true,
-        speed: 300,
-        dots: true,
-      },
-    },
-  ],
-};
