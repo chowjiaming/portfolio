a39c04091644de6f0148922ed8e0031e7d274671
Joseph Chow <contact@josephchow.dev>
2023-02-20 02:18:39 -0500

    :heavy_minus_sign: removed sidebar nav package :heavy_minus_sign:

--- components/Sidebar/SidebarBody.tsx
+++ components/Sidebar/SidebarBody.tsx
@@ -2,12 +2,10 @@ import {Flex, VStack} from '@chakra-ui/react';
 import {motion} from 'framer-motion';
 import {nav} from '@/data/sidebar';
 import {sidebarSettings} from '@/utils/settings';
-import ScrollspyNav from 'react-scrollspy-nav';
 import {SidebarNavButton} from '@/components/Sidebar/SidebarNavButton';
+import {SECTION_PADDING} from '@/utils/constants';
 
 export function SidebarBody(): JSX.Element {
-  const sections = nav.map((n) => n.itemName.toLowerCase());
-
   return (
     <Flex
       as={motion.div}
@@ -15,23 +13,17 @@ export function SidebarBody(): JSX.Element {
       animate="open"
       exit="closed"
       variants={sidebarSettings.sideVariants}
-      flex={8}
-      h={'full'}
-      w={'full'}
-      align={'center'}
-      justify={'center'}
+      flex="8"
+      h="full"
+      w="full"
+      align="center"
+      justify="center"
     >
-      <ScrollspyNav
-        scrollTargetIds={sections}
-        activeNavClass="is-active"
-        scrollDuration="100"
-      >
-        <VStack spacing={8} align="stretch">
-          {nav.map((main) => (
-            <SidebarNavButton key={main.itemRoute} {...main} />
-          ))}
-        </VStack>
-      </ScrollspyNav>
+      <VStack spacing={SECTION_PADDING} align="stretch">
+        {nav.map((main) => (
+          <SidebarNavButton key={main.itemRoute} {...main} />
+        ))}
+      </VStack>
     </Flex>
   );
 }
