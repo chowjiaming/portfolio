3a6188a5210958fe9f9d97df6c7452cb3dcb8b90
Joseph Chow <contact@josephchow.dev>
2023-02-24 19:10:35 -0500

    refactor: new project grid wip :construction:

--- components/Portfolio/Project.tsx
+++ components/Portfolio/Project.tsx
@@ -1,22 +1,24 @@
-import {SECTION_PADDING} from '@/utils/constants';
 import {
-  Box,
-  Tooltip,
+  Card,
+  CardBody,
   chakra,
   shouldForwardProp,
-  useDisclosure,
-  Button,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  Text,
   Heading,
+  Text,
+  Button,
+  Grid,
+  GridItem,
   Flex,
+  IconButton,
+  ButtonGroup,
+  Link,
 } from '@chakra-ui/react';
+import {
+  LARGE_MARGIN,
+  SECTION_PADDING,
+  STANDARD_MARGIN,
+} from '@/utils/constants';
+import {FaCode, FaExternalLinkAlt} from 'react-icons/fa';
 import Image from 'next/image';
 
 const PortfolioImage = chakra(Image, {
@@ -25,114 +27,90 @@ const PortfolioImage = chakra(Image, {
     prop === 'width' ||
     prop === 'height' ||
     prop === 'alt',
-  baseStyle: {
-    _hover: {
-      transform: 'scale(1.1)',
-      transition: 'all 0.3s ease-in-out',
-    },
-  },
 });
 
 type ProjectProps = {
-  name: string;
-  screenshot: string;
-  shortDesc: string;
-  longDesc: string;
+  title: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+  imageWidth: number;
+  imageHeight: number;
+  link: string;
+  skills: string[];
 };
 export function Project({
-  name,
-  screenshot,
-  shortDesc,
-  longDesc,
+  title,
+  description,
+  image,
+  imageAlt,
+  imageWidth,
+  imageHeight,
+  link,
+  skills,
 }: ProjectProps): JSX.Element {
-  const {isOpen, onOpen, onClose} = useDisclosure();
-
   return (
-    <>
-      <Box w={'full'} h={'full'} p={SECTION_PADDING}>
-        <Tooltip label={name} aria-label={name}>
-          <PortfolioImage
-            src={screenshot}
-            alt={name}
-            width={1920}
-            height={926}
-            css={{
-              height: 'auto',
-              width: 'auto',
-            }}
-          />
-        </Tooltip>
-        <Text>{shortDesc}</Text>
-        <Button onClick={onOpen}>View Project</Button>
-      </Box>
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        motionPreset={'slideInBottom'}
-        size={{
-          base: 'xl',
-          md: 'xl',
-          lg: '2xl',
-          xl: '3xl',
+    <Card as="article" h="full" align="center" justify="center" direction="row">
+      <CardBody
+        as={Grid}
+        templateColumns={{
+          base: 'repeat(1, 1fr)',
+          '2xl': 'repeat(2, 1fr)',
         }}
-        isCentered
+        gap={SECTION_PADDING}
       >
-        <ModalOverlay />
-        <ModalCloseButton />
-        <ModalContent>
+        <GridItem as={Flex} justify="center">
           <PortfolioImage
-            src={screenshot}
-            alt={name}
-            width={1920}
-            height={926}
-            css={{
-              height: 'auto',
+            src={image}
+            alt={imageAlt}
+            width={imageWidth}
+            height={imageHeight}
+            style={{
               width: 'auto',
+              height: 'auto',
             }}
           />
-          <ModalHeader>
-            <Heading>{name}</Heading>
-          </ModalHeader>
-
-          <ModalBody>
-            <ModalCloseButton />
-            <Flex
-              direction={{
-                base: 'column',
-                md: 'row',
-              }}
-              gap={{
-                base: 0,
-                md: SECTION_PADDING,
-              }}
-            >
-              <Box flex={3}>
-                <Text>{longDesc}</Text>
-              </Box>
-              <Box flex={1}>
-                <Heading as={'h3'} size={'md'}>
-                  Technologies
-                </Heading>
-                <Text>React</Text>
-                <Text>Next.js</Text>
-                <Text>Chakra UI</Text>
-                <Heading as={'h3'} size={'md'}>
-                  Links
-                </Heading>
-                <Text>GitHub</Text>
-                <Text>Live</Text>
-              </Box>
+        </GridItem>
+        <GridItem
+          as={Flex}
+          direction="column"
+          gap={LARGE_MARGIN}
+          align="center"
+        >
+          <Heading as="h2" size="lg" textAlign="center">
+            {title}
+          </Heading>
+          <Text textAlign="justify">{description}</Text>
+          {skills.length > 0 && (
+            <Flex gap={STANDARD_MARGIN} flexWrap="wrap" justify="center">
+              {skills.map((skill) => (
+                <GridItem key={skill}>
+                  <Button variant="outline" colorScheme="teal" size="sm">
+                    {skill}
+                  </Button>
+                </GridItem>
+              ))}
             </Flex>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
+          )}
+          <ButtonGroup mt="auto">
+            <IconButton
+              as={Link}
+              href={link}
+              aria-label="View project"
+              icon={<FaExternalLinkAlt />}
+              isExternal
+            />
+            <IconButton
+              as={Link}
+              href={link}
+              aria-label="View project code"
+              icon={<FaCode />}
+              isExternal
+            />
+          </ButtonGroup>
+        </GridItem>
+      </CardBody>
+    </Card>
   );
 }
 
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,9 +1,9 @@
 import {SECTION_PADDING} from '@/utils/constants';
-import {Flex} from '@chakra-ui/react';
-import {intro} from '@/data/portfolio';
+import {Flex, Grid, GridItem} from '@chakra-ui/react';
+import {intro, content} from '@/data/portfolio';
 import {SectionHeader} from '@/components/Shared/SectionHeader';
 import {Meta} from '@/components/Meta';
-import {ProjectTabs} from '@/components/Portfolio/ProjectTabs';
+import {Project} from '@/components/Portfolio/Project';
 
 export default function Portfolio(): JSX.Element {
   return (
@@ -17,7 +17,28 @@ export default function Portfolio(): JSX.Element {
         minH="100vh"
       >
         <SectionHeader heading={intro.heading} paragraph={intro.paragraph} />
-        <ProjectTabs />
+        <Grid
+          templateColumns={{
+            base: 'repeat(1, 1fr)',
+            lg: 'repeat(2, 1fr)',
+          }}
+          gap={SECTION_PADDING}
+        >
+          {content.map((project) => (
+            <GridItem key={project.title}>
+              <Project
+                title={project.title}
+                description={project.longDesc}
+                image={project.image}
+                imageAlt={`Screenshot of ${project.title} website`}
+                imageWidth={project.imageWidth}
+                imageHeight={project.imageHeight}
+                link={project.link}
+                skills={project.skills}
+              />
+            </GridItem>
+          ))}
+        </Grid>
       </Flex>
     </>
   );
