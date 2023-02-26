64e4790fb87c73cea924409ed730fc13b5ea576b
Joseph Chow <contact@josephchow.dev>
2023-02-26 13:38:13 -0500

    refactor: added project and skills api wip :building_construction:

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -1,20 +1,11 @@
+import {type Project} from '@/utils/types';
+
 export const intro = {
   heading: 'Portfolio',
   paragraph:
     'Here is a showcase of my work and demonstrate the impact that I have made as a front-end developer. Each project is a testament to my skills, creativity, and commitment to delivering high-quality solutions. I have had the opportunity to work on a variety of projects, ranging from a simple SPA to complex web applications, and have consistently exceeded expectations. My projects speak for themselves, and I am confident that they will give you a good idea of the level of quality that I can bring to your team. Take a look and see what I can do for you!',
 };
 
-export type Project = {
-  title: string;
-  image: string;
-  imageWidth: number;
-  imageHeight: number;
-  github: string;
-  link: string;
-  shortDesc: string;
-  longDesc: string;
-  skills: string[];
-};
 export const content: Project[] = [
   {
     title: 'Tip Calculator App',
--- utils/types.ts
+++ utils/types.ts
@@ -1,3 +1,22 @@
+export type Project = {
+  title: string;
+  image: string;
+  imageWidth: number;
+  imageHeight: number;
+  github: string;
+  link: string;
+  shortDesc: string;
+  longDesc: string;
+  skills: string[];
+};
+
+export type Skill = {
+  title: string;
+  logo: string;
+  // url: string;
+  // description: string;
+};
+
 export type ContactFormTypes = {
   name: string;
   email: string;
